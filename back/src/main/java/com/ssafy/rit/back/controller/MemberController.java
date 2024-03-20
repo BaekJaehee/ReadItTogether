6a676cf12402ecee8e3686ac03f2fc315fc5e704
문수민 <sumny1010@gmail.com>
2024-03-20 09:21:06 +0900

    feat: 응답 메시지 수정

--- back/src/main/java/com/ssafy/rit/back/controller/MemberController.java
+++ back/src/main/java/com/ssafy/rit/back/controller/MemberController.java
@@ -21,13 +21,13 @@ public class MemberController {
         System.out.println("이멜: " + dto.getEmail());
         memberService.signUp(dto);
 
-        return "가입 완료염ㅋㅋ";
+        return "SignUp Success";
     }
 
     @GetMapping("/test")
     public String test() {
-        System.out.println("이거맞나~?");
-        return "허가증 가져왔구나";
+        System.out.println("테스트입니당나라송나라");
+        return "테통. 테스트 통과라는 뜻";
     }
 
 
