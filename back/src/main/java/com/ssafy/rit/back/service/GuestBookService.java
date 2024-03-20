f55e14ffd3e423c3908af8cd049b5f5abe263740
hyoseon <wink2090@naver.com>
2024-03-20 14:37:57 +0900

    feat: 방명록 삭제 api
    방명록 삭제 api 만들었어요

--- back/src/main/java/com/ssafy/rit/back/service/GuestBookService.java
+++ back/src/main/java/com/ssafy/rit/back/service/GuestBookService.java
@@ -3,6 +3,7 @@ package com.ssafy.rit.back.service;
 import com.ssafy.rit.back.dto.guestBook.requestDto.GuestBookCreationRequestDto;
 import com.ssafy.rit.back.dto.guestBook.response.GuestBookCreationResponse;
 import com.ssafy.rit.back.dto.guestBook.response.GuestBookDetailResponse;
+import com.ssafy.rit.back.dto.guestBook.response.GuestBookRemovalResponse;
 import org.springframework.http.ResponseEntity;
 
 public interface GuestBookService {
@@ -10,4 +11,6 @@ public interface GuestBookService {
     ResponseEntity<GuestBookCreationResponse> createGuestBook(Long toMemberId, GuestBookCreationRequestDto dto);
 
     ResponseEntity<GuestBookDetailResponse> readGuestBookDetail(Long postId);
+
+    ResponseEntity<GuestBookRemovalResponse> deleteGuestBook(Long postId);
 }
