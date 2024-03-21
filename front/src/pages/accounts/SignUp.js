5fcb6c2bf61553fcc9aabb34a7dc02c39a48fa3f
Anhyunsung <gustjd980112@yu.ac.kr>
2024-03-21 16:15:10 +0900

    feat: 드디어 해결했다.. 로그인api....

--- front/src/pages/accounts/SignUp.js
+++ front/src/pages/accounts/SignUp.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { handleSignUp } from "../../api/accounts/SignUp";
+import { handleSignUp } from "../../api/accounts/signUp";
 import { checkEmailDuplicate } from "../../api/accounts/MailDuplicate";
 import { checkNicknameDuplicate } from "../../api/accounts/NicknameDuplicate";
 
