c906019a9847e4d9a3df8ee07c7fa6074684634e
hyoseon <wink2090@naver.com>
2024-03-22 09:31:51 +0900

    feat: 우편함 카드 조회 api
    우편함에서 카드 받는거 눌렀을 때, 이번주에 받은 우편 없으면 새로 만들어서 저장 후에 보여주고, 있으면 그거 불러와서 보여줍니다.

--- back/src/main/java/com/ssafy/rit/back/serviceImpl/PostBoxServiceImpl.java
+++ back/src/main/java/com/ssafy/rit/back/serviceImpl/PostBoxServiceImpl.java
@@ -4,13 +4,20 @@ import com.ssafy.rit.back.dto.postBox.response.PostBoxListResponse;
 import com.ssafy.rit.back.dto.postBox.responseDto.PostBoxListResponseDto;
 import com.ssafy.rit.back.dto.postBox.responseDto.ReceiveCardsDto;
 import com.ssafy.rit.back.entity.Card;
+import com.ssafy.rit.back.entity.Member;
+import com.ssafy.rit.back.entity.Postbox;
 import com.ssafy.rit.back.repository.CardRepository;
+import com.ssafy.rit.back.repository.PostBoxRepository;
 import com.ssafy.rit.back.service.PostBoxService;
+import com.ssafy.rit.back.util.CommonUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.temporal.TemporalAdjusters;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -18,24 +25,74 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class PostBoxServiceImpl implements PostBoxService {
 
-    private final CardRepository cardRepository;
+    private static final int POSTBOX_CARD_LIMIT = 3;
 
+    private final CardRepository cardRepository;
+    private final CommonUtil commonUtil;
+    private final PostBoxRepository postBoxRepository;
+
+    // 우편함 조회 로직
     @Override
     public ResponseEntity<PostBoxListResponse> readPostBoxList() {
 
-        List<Card> cards = cardRepository.findRandomCards();
+        Member currentMember = commonUtil.getMember();
 
-        List<ReceiveCardsDto> cardDtos = cards.stream()
+        LocalDate today = LocalDate.now();
+        LocalDate startOfWeek = today.with(TemporalAdjusters.previousOrSame(DayOfWeek.SUNDAY));
+        LocalDate endOfWeek = today.with(TemporalAdjusters.nextOrSame(DayOfWeek.SATURDAY));
+
+        List<Postbox> weeklyPostboxes = postBoxRepository.findAllByMemberIdAndCreationDateBetween(currentMember, startOfWeek, endOfWeek);
+
+        // 이번주에 받은 우편이 없을 경우
+        if (weeklyPostboxes.isEmpty()) {
+            // TODO: Random 추천에서 추천 시스템 로직 적용으로 변경해야 합니다.
+            List<Card> cards = cardRepository.findRandomCards();
+
+            List<ReceiveCardsDto> cardsDto = convertCardsToDto(cards);
+            createPostboxesForCurrentMember(cards, currentMember);
+
+            PostBoxListResponse response = new PostBoxListResponse("카드 조회 성공", new PostBoxListResponseDto(cardsDto));
+
+            return ResponseEntity.status(HttpStatus.OK).body(response);
+        }
+        // 이번주에 받은 우편이 있을 경우
+        else {
+            List<ReceiveCardsDto> cardsDto = weeklyPostboxes.stream()
+                    .map(this::convertPostboxToDto)
+                    .collect(Collectors.toList());
+
+            PostBoxListResponse response = new PostBoxListResponse("카드 조회 성공", new PostBoxListResponseDto(cardsDto));
+
+            return ResponseEntity.status(HttpStatus.OK).body(response);
+        }
+    }
+
+    private ReceiveCardsDto convertPostboxToDto(Postbox postbox) {
+        Card card = postbox.getCardId();
+        return ReceiveCardsDto.builder()
+                .cover(card.getBookId().getCover())
+                .cardId(card.getId())
+                .build();
+    }
+
+    private List<ReceiveCardsDto> convertCardsToDto(List<Card> cards) {
+        return cards.stream()
                 .map(card -> ReceiveCardsDto.builder()
                         .cover(card.getBookId().getCover())
                         .cardId(card.getId())
                         .build())
                 .collect(Collectors.toList());
+    }
 
-        PostBoxListResponseDto detailDto = new PostBoxListResponseDto(cardDtos);
-
-        PostBoxListResponse response = new PostBoxListResponse("카드 조회 성공", detailDto);
-
-        return ResponseEntity.status(HttpStatus.OK).body(response);
+    private void createPostboxesForCurrentMember(List<Card> cards, Member currentMember) {
+        cards.stream().limit(PostBoxServiceImpl.POSTBOX_CARD_LIMIT)
+                .forEach(card -> {
+                    Postbox newPostbox = Postbox.builder()
+                            .createdAt(LocalDate.now())
+                            .cardId(card)
+                            .memberId(currentMember)
+                            .build();
+                    postBoxRepository.save(newPostbox);
+                });
     }
 }
