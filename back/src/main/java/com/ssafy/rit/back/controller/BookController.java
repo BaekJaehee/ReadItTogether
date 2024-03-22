09b2efd0a589860b20d824790133c89061bc50d9
Lim Kyuhwan <cnn482@gmail.com>
2024-03-22 17:59:55 +0900

    feat: 삭제 빼고 다

--- back/src/main/java/com/ssafy/rit/back/controller/BookController.java
+++ back/src/main/java/com/ssafy/rit/back/controller/BookController.java
@@ -1,9 +1,8 @@
 package com.ssafy.rit.back.controller;
 
 import com.ssafy.rit.back.dto.book.requestDto.CommentCreationRequestDto;
-import com.ssafy.rit.back.dto.book.response.BookDetailResponse;
-import com.ssafy.rit.back.dto.book.response.CommentCreationResponse;
-import com.ssafy.rit.back.dto.book.response.CommentListResponse;
+import com.ssafy.rit.back.dto.book.requestDto.CommentUpdateRequestDto;
+import com.ssafy.rit.back.dto.book.response.*;
 import com.ssafy.rit.back.service.BookService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -25,18 +24,28 @@ public class BookController {
         return bookService.readBookDetail(bookId);
     }
 
-
     // 코멘트(리뷰) 관련 CRUD
     // 코메트 작성
     @PostMapping("/comment")
     public ResponseEntity<CommentCreationResponse> createComment(@RequestBody CommentCreationRequestDto dto){
         return bookService.createComment(dto);
-
     }
-
+    // 코멘트 조회
     @GetMapping("/comment/{bookId}")
     public ResponseEntity<CommentListResponse> readCommentList(@PathVariable("bookId") int bookId) {
         return bookService.readCommentList(bookId);
     }
+    // 코멘트 수정
+    @PatchMapping("/comment")
+    public ResponseEntity<CommentUpdateResponse> updateComment(CommentUpdateRequestDto dto) {
+        return bookService.updateComment(dto);
+    }
+
+    // 코멘트 삭제
+    @DeleteMapping("/comment/{commentId}")
+    public ResponseEntity<CommentDeleteResponse> deleteComment(@PathVariable("commentId") Long commentId) {
+        return bookService.deleteComment(commentId);
+    }
+
 
 }
