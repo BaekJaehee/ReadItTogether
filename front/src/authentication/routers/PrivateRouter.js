49149f8331c3429a5c8489761dcf068dc4adf234
Anhyunsung <gustjd980112@yu.ac.kr>
2024-03-25 14:20:48 +0900

    user auth Complete.

--- front/src/authentication/routers/PrivateRouter.js
+++ front/src/authentication/routers/PrivateRouter.js
@@ -3,9 +3,9 @@ import { Navigate, Outlet } from "react-router-dom";
 import { AuthContext } from "../AuthContext";
 
 const PrivateRoute = () => {
-  const { setUserState } = useContext(AuthContext);
+  const { userState } = useContext(AuthContext);
 
-  return setUserState.status === "loggedIn" ? (
+  return userState.status === "loggedIn" ? (
     <Outlet />
   ) : (
     <Navigate to="/login" />
