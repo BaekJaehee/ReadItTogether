ef510501613e71d3ad55993e91892fdfab6213d7
복영석 <dudtjr856@gmail.com>
2024-03-25 14:45:16 +0900

    Delete CardService.java

--- back/src/main/java/com/ssafy/rit/back/service/CardService.java
+++
@@ -1,8 +0,0 @@
-package com.ssafy.rit.back.service;
-
-import com.ssafy.rit.back.dto.card.requestDto.CardCreationRequestDto;
-import com.ssafy.rit.back.entity.Card;
-
-public interface CardService {
-    Card createCard(CardCreationRequestDto requestDto);
-}
