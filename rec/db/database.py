37ecd98d1bd59c4c6ef54a88092955998766a56d
hyoseon <wink2090@naver.com>
2024-03-26 09:30:54 +0900

    feat: database connect
    fastapi db연결

--- rec/db/database.py
+++ rec/db/database.py
@@ -1,17 +1,31 @@
-# database.py
-from databases import Database
-from typing import List
+import aiomysql
+from typing import Optional
 
 # 데이터베이스 연결 정보 설정
-DATABASE_URL = "mysql://root:!A123456bc@34.64.150.109/ssafy"
+DATABASE_CONFIG = {
+    'host': 'localhost',
+    'port': 3306,
+    'user': 'root',
+    'password': 'qwer1234!',
+    'db': 'testdb',
+}
 
-# Database 인스턴스 생성
-database = Database(DATABASE_URL)
+class Database:
+    def __init__(self):
+        self.pool: Optional[aiomysql.Pool] = None
 
-async def fetch_members() -> List[dict]:
-    query = "SELECT * FROM member"
-    return await database.fetch_all(query)
+    async def connect(self):
+        self.pool = await aiomysql.create_pool(**DATABASE_CONFIG)
 
-async def fetch_comments() -> List[dict]:
-    query = "SELECT member_id, book_id, rating FROM comment"
-    return await database.fetch_all(query)
+    async def disconnect(self):
+        self.pool.close()
+        await self.pool.wait_closed()
+
+    async def fetch_comments(self):
+        async with self.pool.acquire() as conn:
+            async with conn.cursor(aiomysql.DictCursor) as cur:
+                await cur.execute("SELECT * FROM comments")
+                comments = await cur.fetchall()
+                return comments
+
+database = Database()
