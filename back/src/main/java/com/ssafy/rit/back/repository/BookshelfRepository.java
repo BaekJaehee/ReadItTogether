b2ca2a3fabbb20bebfa203e9e8031aa4ef368208
Lim Kyuhwan <cnn482@gmail.com>
2024-03-27 17:50:33 +0900

    dd

--- back/src/main/java/com/ssafy/rit/back/repository/BookshelfRepository.java
+++ back/src/main/java/com/ssafy/rit/back/repository/BookshelfRepository.java
@@ -5,10 +5,13 @@ import com.ssafy.rit.back.entity.Bookshelf;
 import com.ssafy.rit.back.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface BookshelfRepository extends JpaRepository <Bookshelf, Integer> {
 
     Optional<Bookshelf> findByBookIdAndMemberId(Book bookId, Member memberId);
+
+    List<Bookshelf> findAllByMemberId(Member member);
 
 }
