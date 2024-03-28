50ae8de81317dddff6e8c9fefdef6eb83baa369c
문수민 <sumny1010@gmail.com>
2024-03-28 09:17:59 +0900

    feat: 로그인 시 응답 바디에 프로필 이미지, 닉네임 추가

--- back/src/main/java/com/ssafy/rit/back/security/filter/LoginFilter.java
+++ back/src/main/java/com/ssafy/rit/back/security/filter/LoginFilter.java
@@ -80,6 +80,8 @@ public class LoginFilter extends UsernamePasswordAuthenticationFilter {
         String email = authentication.getName();
         Member member = memberRepository.findByEmail(email).orElseThrow(MemberNotFoundException::new);
         Long memberId = member.getId();
+        String profileImage = member.getProfileImage();
+        String nickname = member.getNickname();
 
 
         String accessToken = jwtUtil.createJwt("Authorization", email, 600000L);
@@ -95,7 +97,7 @@ public class LoginFilter extends UsernamePasswordAuthenticationFilter {
 
         log.info("-----------------------로그인 완료염-----------------------");
 
-        DataDto dataDto = new DataDto(accessToken, refreshToken, memberId);
+        DataDto dataDto = new DataDto(accessToken, refreshToken, memberId, profileImage, nickname);
         SignInResponseDto responseDto = new SignInResponseDto("Login Success", dataDto);
 
         ObjectMapper objectMapper = new ObjectMapper();
