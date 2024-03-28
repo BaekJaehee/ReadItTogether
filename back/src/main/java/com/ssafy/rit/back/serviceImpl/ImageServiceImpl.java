38e229d6174bede2421ab766ba9c16a350674f88
문수민 <sumny1010@gmail.com>
2024-03-28 15:56:21 +0900

    feat: 가입 시 기본 프로필 사진

--- back/src/main/java/com/ssafy/rit/back/serviceImpl/ImageServiceImpl.java
+++ back/src/main/java/com/ssafy/rit/back/serviceImpl/ImageServiceImpl.java
@@ -34,7 +34,7 @@ public class ImageServiceImpl implements ImageService {
     private String bucket;
 
     // 굳이 해야 하나?
-    private String localLocation = "C:\\Users\\SSAFY\\Desktop\\beforebucket\\";
+    private String localLocation = System.getProperty("java.io.tmpdir");
 
     @Override
     @Transactional
