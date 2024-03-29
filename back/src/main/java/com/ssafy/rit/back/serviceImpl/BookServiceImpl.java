1c1ed1e4de59c97832f923c5f93f3badf09a4082
Lim Kyuhwan <cnn482@gmail.com>
2024-03-29 14:19:58 +0900

    fix: 평점

--- back/src/main/java/com/ssafy/rit/back/serviceImpl/BookServiceImpl.java
+++ back/src/main/java/com/ssafy/rit/back/serviceImpl/BookServiceImpl.java
@@ -209,13 +209,13 @@ public class BookServiceImpl implements BookService {
         }
 
         // comment entity 에 저장되어있는 BookId는 사실 Book 전체 정보이다 (추후에 객체 이름 수정 필요)
-        Book currentBook = comment.getBookId();
+//        Book currentBook = comment.getBookId();
 
         // 삭제
         commentRepository.delete(comment);
 
-        log.info("(3) {}", currentBook.getId());
-        this.updateBookRating(currentBook.getId());
+//        log.info("(3) {}", currentBook.getId());
+        this.updateBookRating(comment.getBookId().getId());
 
         CommentDeleteResponse response = CommentDeleteResponse.builder()
                 .message("코멘트 삭제 성공")
@@ -230,6 +230,10 @@ public class BookServiceImpl implements BookService {
         Integer averageRating = bookRepository.findAverageRatingByBookId(bookId);
         Book book = bookRepository.findById(bookId).orElseThrow(() -> new EntityNotFoundException("Book not found"));
         book.setRating(averageRating);
+
+        Integer countComment = bookRepository.findCountCommentByBookId(bookId);
+        book.setReviewerCnt(countComment);
+
         bookRepository.save(book);
     }
 
