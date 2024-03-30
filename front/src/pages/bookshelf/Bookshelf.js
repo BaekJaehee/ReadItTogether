3dd7a53d2f39a505d750ce388f5ecee63b317fc3
BaekJaehee <151-51@naver.com>
2024-03-30 22:14:14 +0900

    feat: 검색 & 토글 시 검색 초기화 & 상단 이동 버튼

--- front/src/pages/bookshelf/Bookshelf.js
+++ front/src/pages/bookshelf/Bookshelf.js
@@ -23,6 +23,17 @@ const Bookshelf = () => {
 
   // const isReadOrNot = isRead === false ? '읽은 책 보기' : '읽을 책 보기';
 
+  // 읽은 책으로 - 읽을 책으로 버튼 토글 -> O
+  // isRead 기준으로 책 컴포넌트 구분(토글) -> O
+  // 검색(GetBookshelfList searchKeyword) -> O
+  // 정렬(별점순 제거, 출간일 -> 등록순, 0: 최신등록순(default))
+  // 무한스크롤 -> O
+
+  // 기본 컴포넌트가 설정했는데 안나옴 -> 왜 되지
+  // 무한스크롤 추가하고 컴포넌트 토글이 안됨 -> 왜 되지
+  // 무한스크롤 시 맨 위로 올라가는 버튼(화면 하단 붙박이) 필요 -> O
+
+
   useEffect(() => {
     const pathArray = location.pathname.split("/");
     const whoMemberId = pathArray[pathArray.length - 1];
@@ -66,22 +77,48 @@ const Bookshelf = () => {
   
   // 무한스크롤에서도 토글이 되도록
   const toggleRead = () => {
-    setIsRead((prevIsRead) => !prevIsRead);
-    setCurrentComponent((prevComponent) => (prevComponent === 'NotRead' ? 'Read' : 'NotRead'));
-    setSearchKeyword(''); // 검색어 초기화
-    setPage(0); // 페이지 초기화
-    setSort(0); // 정렬 초기화
-    // 현재 컴포넌트에 해당하는 데이터 다시 가져오기
-    const fetchData = async () => {
-      try {
-        const response = await GetBookShelfList(toMemberId, 0, 10, sort, '');
-        setBookshelfInfo(response.data.data);
-      } catch (error) {
-        console.error('데이터를 가져오는 데 실패했습니다:', error);
-      }
-    };
-    fetchData();
+    if (!isSearching) {
+      setIsSearching(true); // 중복 호출 방지를 위해 검색 중인지 여부를 확인합니다.
+      setIsRead(prevIsRead => !prevIsRead); // 읽은 책/읽을 책 토글
+  
+      setCurrentComponent(prevComponent => (prevComponent === 'NotRead' ? 'Read' : 'NotRead'));
+      setSearchKeyword(''); // 검색어 초기화
+      setPage(0); // 페이지 초기화
+      setSort(0); // 정렬 초기화
+  
+      // 현재 컴포넌트에 해당하는 데이터 다시 가져오기
+      const fetchData = async () => {
+        try {
+          const response = await GetBookShelfList(toMemberId, 0, 10, sort, '');
+          setBookshelfInfo(response.data.data);
+        } catch (error) {
+          console.error('데이터를 가져오는 데 실패했습니다:', error);
+        } finally {
+          setIsSearching(false); // 검색 완료 후 상태를 다시 false로 변경합니다.
+        }
+      };
+      fetchData();
+    }
   };
+  
+
+  // const toggleRead = () => {
+  //   setIsRead((prevIsRead) => !prevIsRead);
+  //   setCurrentComponent((prevComponent) => (prevComponent === 'NotRead' ? 'Read' : 'NotRead'));
+  //   setSearchKeyword(''); // 검색어 초기화
+  //   setPage(0); // 페이지 초기화
+  //   setSort(0); // 정렬 초기화
+  //   // 현재 컴포넌트에 해당하는 데이터 다시 가져오기
+  //   const fetchData = async () => {
+  //     try {
+  //       const response = await GetBookShelfList(toMemberId, 0, 10, sort, '');
+  //       setBookshelfInfo(response.data.data);
+  //     } catch (error) {
+  //       console.error('데이터를 가져오는 데 실패했습니다:', error);
+  //     }
+  //   };
+  //   fetchData();
+  // };
 
   useEffect(() => {
     const fetchBookshelfList = async () => {
