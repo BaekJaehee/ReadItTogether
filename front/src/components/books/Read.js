90eee2bea86a5cb484ab4e971322bd22f92b918a
BaekJaehee <151-51@naver.com>
2024-03-31 00:44:25 +0900

    feat: 책 상태 변경

--- front/src/components/books/Read.js
+++ front/src/components/books/Read.js
@@ -1,18 +1,29 @@
 import React, { useState, useEffect } from "react";
 
+import UpdateBook from "../../api/book/bookshelf/UpdateBook";
 // isRead 1
 
-const Read = ({ bookshelfInfo, handleClickBook, handleScroll  }) => {
+const Read = ({ bookshelfInfo, handleClickBook, handleScroll, updateBookshelfInfo }) => {
+  const switchBook = async (bookId) => {
+    try {
+      await UpdateBook(bookId);
+      updateBookshelfInfo();
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   return (
     <div>
       <div className="grid grid-cols-5 gap-5">
-        {Object.values(bookshelfInfo).map((book) => (
+        {/* {Object.values(bookshelfInfo).map((book) => ( */}
+        {bookshelfInfo.map((book) => (
           <div key={book.bookId} className="text-center">
             <div className="m-3 cursor-pointer" onClick={() => handleClickBook(book.bookId)}>
               <img src={book.cover} alt={book.title} className="w-48 h-72" />
               <p className="m-1 font-bold">{book.title}</p>
             </div>
-            <button className="bg-green-500 hover:bg-green-700 text-white py-2 px-4 rounded-full">읽을 책으로</button>
+            <button className="bg-green-500 hover:bg-green-700 text-white py-2 px-4 rounded-full" onClick={() => switchBook(book.bookId)}>읽을 책으로</button>
           </div>
         ))}
       </div>
