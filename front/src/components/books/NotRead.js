73cb5913ef4057e58d0ec20fa462e5239a8df4b3
dudtjr <dudtjr856@gmail.com>
2024-04-01 17:57:09 +0900

    123

--- front/src/components/books/NotRead.js
+++ front/src/components/books/NotRead.js
@@ -1,17 +1,25 @@
-const NotRead = ({ bookshelfInfo, handleClickBook, handleUpdateBookshelf }) => {
+import deleteButton from "../../assets/profile/delete.png"
+
+// In Read component
+// In Read component
+const NotRead = ({ bookshelfInfo, handleClickBook, handleUpdateBookshelf, handleDeleteBookshelf }) => {
   return (
     <div className="grid grid-cols-5 gap-5">
       {bookshelfInfo.map((book) => (
-        <div key={book.bookId} className="text-center">
+        <div key={book.bookId} className="text-center relative group">
+          <div className="absolute right-2 top-2 p-2 cursor-pointer opacity-0 group-hover:opacity-100" 
+               onClick={() => handleDeleteBookshelf(book.book)}>
+            <img className="w-4 h-4" src={deleteButton} alt="삭제버튼" />
+          </div>
           <div className="m-3 cursor-pointer" onClick={() => handleClickBook(book.bookId)}>
             <img src={book.cover} alt={book.title} className="w-48 h-72" />
             <p className="m-1 font-bold">{book.title}</p>
           </div>
           <button 
-            className="bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded-full"
+            className="bg-green-500 hover:bg-green-700 text-white py-2 px-4 rounded-full mt-2"
             onClick={() => handleUpdateBookshelf(book.bookId)}
           >
-            읽은 책으로
+            읽을 책으로
           </button>
         </div>
       ))}
@@ -20,3 +28,4 @@ const NotRead = ({ bookshelfInfo, handleClickBook, handleUpdateBookshelf }) => {
 };
 
 export default NotRead;
+
