9077a02b9b45cb5f462faf8b46a65bd566c790c4
Anhyunsung <gustjd980112@yu.ac.kr>
2024-04-02 17:49:19 +0900

    책장 꾸밈

--- front/src/components/books/NotRead.js
+++ front/src/components/books/NotRead.js
@@ -2,19 +2,19 @@ import deleteButton from "../../assets/profile/delete.png";
 
 const NotRead = ({ books, handleClickBook, handleUpdateBookshelf, handleDeleteBookshelf }) => {
   return (
-    <div className="grid grid-cols-5 gap-5">
+    <div className="grid grid-cols-4">
       {books.map((book) => (
         <div key={book.bookId} className="text-center relative group">
-          <div className="absolute right-2 top-2 p-2 cursor-pointer opacity-0 group-hover:opacity-100" 
+          <div className="absolute right-4 top-4 cursor-pointer opacity-0 group-hover:opacity-100" 
                onClick={() => handleDeleteBookshelf(book.bookshelfId)}>
             <img className="w-4 h-4" src={deleteButton} alt="삭제버튼" />
           </div>
           <div className="m-3 cursor-pointer" onClick={() => handleClickBook(book.bookId)}>
-            <img src={book.cover} alt={book.title} className="flex w-48 h-72 items-center justify-center" />
+            <img src={book.cover} alt={book.title} className="flex w-[310px] h-[440px] items-center justify-center" />
             <p className="m-1 font-bold">{book.title}</p>
           </div>
           <button 
-            className="bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded-full mt-2"
+            className="bg-white border border-blue-300 hover:bg-blue-300 hover:text-white rounded-lg text-blue-500 text-xs py-2 px-4 mt-2 transition-colors duration-300"
             onClick={() => handleUpdateBookshelf(book.bookId)}
           >
             읽은 책으로
